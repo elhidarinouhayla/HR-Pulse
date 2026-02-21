352639e3a906ee21274fe7059b8408ce8da6a6ee
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-21 23:21:33 +0000

    entrainement du model

--- backend/ml/train.py
+++ backend/ml/train.py
@@ -1,6 +1,68 @@
-import pandas as pd
-from preprocessing import clean_salary
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
 
-df = pd.read_csv("../data/raw/jobs_data.csv")
 
-df["salary_clean"] = df["Salary Estimate"].apply(clean_salary)
+# split du data
+
+def split_data(df_clean, target='salary_clean', test_size=0.2):
+
+    x = df_clean.drop(columns=[target])
+    y = df_clean[target]
+
+    x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=test_size, random_state=42)
+
+    return x_train, y_train, x_test, y_test
+
+
+# realisation du pipeline sklearn
+
+def pipeline(numerical_cols, categorical_cols, model):
+
+    numeric_transform = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler())
+    ])
+
+    categorical_transform = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transform, numerical_cols),
+            ("cat", categorical_transform, categorical_cols)
+        ]
+    )
+
+    return Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("model", model)
+        ]
+    )
+
+
+# metriques d'evaluation
+
+def metric_model(y_test, y_pred):
+
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_absolute_error(y_test, y_pred)
+
+    return r2, mae, mse
+
+
+
+
+
+
+
+
+
+

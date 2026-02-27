1ac5b20451fc34e5d479f942fdde9b17096aa658
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-27 10:03:19 +0000

    create job_offers table

--- backend/models/users_model.py
+++ backend/models/users_model.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from backend.database import Base
 
 class JobOffer(Base):
     __tablename__ = "job_offers"
 
     id = Column(Integer, primary_key=True, index=True)
-    job_title = Column(String(255), nullable=False)
+    role = Column(String, nullable=False)
     skills_extracted = Column(Text, nullable=False)  

74442c11c8240d19f8f161aa9b6e590117cb6dc3
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-27 10:08:00 +0000

    create prediction endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,28 @@
-from fastapi import FastAPI
-from predictor import predict_salary
+from fastapi import FastAPI, HTTPException
+from backend.services.pedict_service import predict_salary
+from backend.services.skills_model import run_ner_extraction, authenticate_client
+from backend.schemas.extract_skills import JobRequest, JobResponse
+from backend.schemas.predict_schema import UserRequest, UserResponse
+from backend.database import Base, engine
+from backend.models import users_model
+from backend.schemas.extract_skills import JobRequest, JobResponse, JobOut
+from typing import List
+from backend.services.extract_skills import get_all_skills, get_jobs_by_skill, extract_skills_from_text
 
-app = FastAPI()
+
 
 Base.metadata.create_all(bind=engine)
 
 
-# creation d'un username :
-@app.post("/register", response_model=UserResponse)
-def create_user(user:UserCreate, db: Session=Depends(get_db)):
-    exist = db.query(User).filter(User.username == user.username).first()
+app = FastAPI()
 
-    if exist:
-        raise HTTPException(status_code=400, detail= "username existe deja")
-    
-    # haching password
-    hashed_pwd = hache_password(user.password)
-    
-    new_user = User(username=user.username, password=hashed_pwd, email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+@app.post("/predict", response_model=UserResponse)
+def predict(data: UserRequest):
+    print(data)
+    data = data.dict()
+    salary = predict_salary(data)
+ 
 
-    return new_user
+    return {"salary": salary}
 
 
-
-
-# verifier l'identifiant et encoder token
-@app.post("/login")
-def login(user:UserVerify, db: Session=Depends(get_db)):
-
-    db_user = db.query(User).filter(
-        User.username == user.username
-        ).first()
-    
-    if not db_user or not verify_password(user.password,db_user.password):
-        raise HTTPException(status_code=400, detail="username or password incorect")
-    
-    token = create_token(db_user.username)
-
-    return {"token" : token}
-
-
-
-
-
-
-
-
-
-@app.post("/predict")
-def predict(data: dict):
-    features = [
-        data["rating"]
-    ]
-    salary = predict_salary(features)
-    return {"predicted_salary": salary}

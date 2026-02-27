07752a6c662d281cd8f3ef451651bd45360b15ef
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-27 10:04:49 +0000

    create JobResponce , JobOut schemas

--- backend/schemas/extract_skills.py
+++ backend/schemas/extract_skills.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
-
+from typing import List
 
 
 class JobRequest(BaseModel):
     job_description: str
+
+
+class JobResponse(BaseModel):
+    skills: List[str]
+
+
+class JobOut(BaseModel):
+    id: int
+    job_title: str
+    skills_extracted: List[str]

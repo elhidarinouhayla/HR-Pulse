c6f31332a82e50bcf91fe23ccfe232e78c608e66
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-27 10:05:24 +0000

    create UserRequest, UserResponse schemas

--- backend/schemas/predict_schema.py
+++ backend/schemas/predict_schema.py
@@ -1,3 +1,16 @@
 from pydantic import BaseModel
 
 
+class UserRequest(BaseModel):
+
+    JobDescription: str
+    location: str
+    role: str
+    ownership_category: str
+    Industry: str
+    Sector: str
+        
+
+
+class UserResponse(BaseModel):
+    salary : float

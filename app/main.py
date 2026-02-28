9761f0d7dc74883c8ebbb5b492c13b70589bb57e
Nouhayla <elhidarinouhayla@gmail.com>
2026-02-28 00:18:33 +0000

    feat: add app/main.py

---
+++ app/main.py
@@ -0,0 +1,123 @@
+from fastapi import FastAPI, HTTPException, Depends
+from app.services.pedict_service import predict_salary
+from app.schemas.predict_schema import PredictRequest, PredictResponse
+from app.database import Base, engine, get_db
+from app.models import jobs_model, users_model
+from sqlalchemy.orm import Session
+from app.schemas.user_schema import UserCreate, UserResponse, UserVerify
+from app.models.users_model import User
+from app.auth import create_token, verify_password, verify_token, hache_password
+from app.schemas.skills_schema import JobRequest, JobResponse, JobOut
+from typing import List
+from app.services.extract_skills import get_all_skills, get_jobs_by_skill
+from fastapi.middleware.cors import CORSMiddleware
+from app.models.jobs_model import JobOffer
+import json
+
+
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+# creation d'un username :
+@app.post("/register", response_model=UserResponse)
+def create_user(user:UserCreate, db: Session=Depends(get_db)):
+    exist = db.query(User).filter(User.username == user.username).first()
+
+    if exist:
+        raise HTTPException(status_code=400, detail= "username existe deja")
+    
+    # haching password
+    hashed_pwd = hache_password(user.password)
+    
+    new_user = User(username=user.username, password=hashed_pwd, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+
+
+# verifier l'identifiant et encoder token
+@app.post("/login")
+def login(user:UserVerify, db: Session=Depends(get_db)):
+
+    db_user = db.query(User).filter(
+        User.username == user.username
+        ).first()
+    
+    if not db_user or not verify_password(user.password,db_user.password):
+        raise HTTPException(status_code=400, detail="username or password incorect")
+    
+    token = create_token(db_user.username)
+
+    return {"token" : token}
+
+
+
+
+# predict salary
+@app.post("/predict", response_model=PredictResponse)
+def predict(data: PredictRequest):
+    print(data)
+    data = data.dict()
+    salary = predict_salary(data)
+
+    return {"salary": salary}
+
+
+# #  extract skills
+# @app.post("/extract_skills", response_model=JobResponse)
+# def extract_skills_endpoint(
+#     request: JobRequest,
+#     db: Session = Depends(get_db)
+# ):
+#     skills = extract_skills(request.job_description, db)
+#     print(skills)
+#     job = JobOffer(
+#         role="Unknown",
+#         skills_extracted=json.dumps(skills)
+#     )
+#     db.add(job)
+#     db.commit()
+
+#     return JobResponse(skills=skills)
+
+
+# skills list
+@app.get("/skills", response_model=list[str])
+def list_skills(db: Session = Depends(get_db)):
+    return get_all_skills(db)
+
+
+@app.get("/jobs")
+def jobs(db: Session = Depends(get_db)):
+    return db.query(JobOffer).all()
+
+
+
+# jobs list
+@app.get("/jobs_by_skill/{skill}")
+def jobs_by_skill(skill: str, db: Session = Depends(get_db)):
+    result = get_jobs_by_skill(db, skill)
+
+    if not result:
+        raise HTTPException(status_code=404, detail="No jobs found")
+
+    return result
